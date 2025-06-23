1743fb64daf761b06d134bc661550715d1ae2cac
AGUSMON10 <agunisa25@gmail.com>
2025-06-23 20:17:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,24 +21,30 @@ if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
 # Lista de ítems con URL y precio máximo aceptado
 skins_a_vigilar = {
     "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Ultraviolet%20%28Well-Worn%29":
-    165.00,
+    144.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Talon%20Knife%20%7C%20Urban%20Masked%20%28Factory%20New%29":
+    390.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Stained%20%28Well-Worn%29":
+    128.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Damascus%20Steel%20%28Factory%20New%29":
+    204.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Night%20Stripe%20%28Minimal%20Wear%29":
+    174.00,
     "https://steamcommunity.com/market/listings/730/%E2%98%85%20Paracord%20Knife%20%7C%20Urban%20Masked%20%28Minimal%20Wear%29":
     163.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Paracord%20Knife%20%7C%20Scorched%20%28Well-Worn%29":
-    143.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Bowie%20Knife%20%7C%20Boreal%20Forest%20%28Well-Worn%29":
-    150.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Falchion%20Knife%20%7C%20Bright%20Water%20%28Minimal%20Wear%29":
-    155.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Forest%20DDPAT%20%28Minimal%20Wear%29":
-    152.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Navaja%20Knife%20%7C%20Blue%20Steel%20%28Minimal%20Wear%29":
-    146.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Ultraviolet%20%28Well-Worn%29":
-    149.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Flip%20Knife%20%7C%20Case%20Hardened%20%28Minimal%20Wear%29":
+    325.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Falchion%20Knife%20%7C%20Damascus%20Steel%20%28Battle-Scarred%29#":
+    177.00,
     "https://steamcommunity.com/market/listings/730/P250%20%7C%20Modern%20Hunter%20%28Well-Worn%29":
     8.00,
-    "https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20AWP%20%7C%20Corticera%20%28Factory%20New%29": 121.00
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Falchion%20Knife%20%7C%20Ultraviolet%20%28Well-Worn%29":
+    160.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Falchion%20Knife%20%7C%20Case%20Hardened%20%28Battle-Scarred%29":
+    250.00,
+    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Falchion%20Knife%20%7C%20Blue%20Steel%20%28Factory%20New%29":
+    313.00,
+    "https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20AWP%20%7C%20Corticera%20%28Factory%20New%29": 123.00
 }
 
 notificados = {}
