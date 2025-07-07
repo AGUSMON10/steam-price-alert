88175660e1e176b5273c20fbab43a363b68da6d0
AGUSMON10 <agunisa25@gmail.com>
2025-07-07 16:00:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,26 +30,8 @@ if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
 
 # Lista de ítems con URL y precio máximo aceptado
 skins_a_vigilar = {
-    "https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20Glock-18%20%7C%20Wasteland%20Rebel%20%28Minimal%20Wear%29":
-    25.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Falchion%20Knife%20%7C%20Case%20Hardened%20%28Battle-Scarred%29":
-    248.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Survival%20Knife%20%7C%20Case%20Hardened%20%28Minimal%20Wear%29":
-    255.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Damascus%20Steel%20%28Factory%20New%29":
-    216.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Falchion%20Knife%20%7C%20Bright%20Water%20%28Factory%20New%29":
-    220.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Paracord%20Knife%20%7C%20Ultraviolet%20%28Well-Worn%29":
-    151.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Gut%20Knife%20%7C%20Blue%20Steel%20%28Factory%20New%29":
-    188.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Paracord%20Knife%20%7C%20Urban%20Masked%20%28Factory%20New%29":
-    235.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Falchion%20Knife%20%7C%20Damascus%20Steel%20%28Battle-Scarred%29":
-    181.00,
-    "https://steamcommunity.com/market/listings/730/%E2%98%85%20Classic%20Knife%20%7C%20Scorched%20%28Minimal%20Wear%29":
-    196.00,
+    "https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20AWP%20%7C%20Corticera%20%28Factory%20New%29":
+    145.00,
     "https://steamcommunity.com/market/listings/730/%E2%98%85%20Specialist%20Gloves%20%7C%20Fade%20%28Battle-Scarred%29":
     230.00,
     "https://steamcommunity.com/market/listings/730/%E2%98%85%20Hand%20Wraps%20%7C%20CAUTION%21%20%28Battle-Scarred%29":
