97a244992606256968c5bda28d34a1c11a8d579f
AGUSMON10 <agunisa25@gmail.com>
2025-07-08 07:53:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,12 @@ skins_a_vigilar = {
     229.00,
     "https://steamcommunity.com/market/listings/730/%E2%98%85%20Driver%20Gloves%20%7C%20Black%20Tie%20%28Battle-Scarred%29":
     205.00,
+    "https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Inheritance%20%28Field-Tested%29":
+    90.00,
+    "https://steamcommunity.com/market/listings/730/Glock-18%20%7C%20Shinobu%20%28Minimal%20Wear%29":
+    12.00,
+    "https://steamcommunity.com/market/listings/730/M4A1-S%20%7C%20Decimator%20%28Minimal%20Wear%29":
+    25.00,
     "https://steamcommunity.com/market/listings/730/%E2%98%85%20StatTrak%E2%84%A2%20Falchion%20Knife%20%7C%20Urban%20Masked%20%28Minimal%20Wear%29": 193.00
 }
 
